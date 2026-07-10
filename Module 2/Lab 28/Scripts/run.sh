cb4e11788509d5931b26d5dbbaae808e31c98328
LIKHITH GOWDA H R <likhithgowdahr005@gmail.com>
2026-07-10 22:48:34 +0530

    Add files via upload

--- Module 2/Lab 28/Scripts/run.sh
+++ Module 2/Lab 28/Scripts/run.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#Step 1: Run the Verilator to compile and genrate simu;ation files 
+verilator --binary -j 0 -Wall async_fifo.v async_fifo_tb.v --top async_fifo_tb --timing --CFLAGS "-std=c++20" --trace
+
+#Step 2: Navigate to the object directory 
+cd obj_dir || { echo "obj_dir not found"; exit 1; }
+
+#Step 3: Compile the simulation binary 
+make -f Vasync_fifo_tb.mk Vasync_fifo_tb 
+
+#Step 4: Run the simulation
+./Vasync_fifo_tb || { echo "Simualtion failed"; exit ; }
+
+#Step : Open the waveform in GTKWave
+gtkwave async_fifo.vcd
+
+
+
+
 
