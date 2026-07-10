1dff8fac66456169286319d7e52996fb38169d69
LIKHITH GOWDA H R <likhithgowdahr005@gmail.com>
2026-07-10 23:19:16 +0530

    Add files via upload

--- Module 2/Lab 29/Scripts/icg_cell/run.sh
+++ Module 2/Lab 29/Scripts/icg_cell/run.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+#1: Run Verilator to compile and genrate simulation files
+verilator --binary -j 0 -Wall icg_cell_model.v icg_cell_model_tb.v --top icg_cell_model_tb --timing --CFLAGS "-std=c++20" --trace
+#2 : Navigate to the object directory
+cd obj_dir || { echo "obj_dir not found"; exit 1; }
+#3: Compile the simualtion binary
+make -f Vicg_cell_model_tb.mk Vicg_cell_model_tb || { echo "Compilation failed"; exit 1; }
+#4: Run the simulation
+./Vicg_cell_model_tb || { echo "Simulation failed"; exit 1; }
+#5: Open the waveform in GTKWave
+gtkwave icg_cell_model.vcd
 
